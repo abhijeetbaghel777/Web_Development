c48e387f86e2024f36b05c6dd64f9f7ed4103893
Abhijeet Singh Baghel <abhijeetbaghel77@gmail.com>
2026-06-06 08:04:52 +0530

    methods/js

---
+++ lecture18/methods.js
@@ -0,0 +1,8 @@
+// og str me koi change nahi hoga
+let str="    js js js      "
+// console.log(str.replaceAll("js","jsx"))// replace all with given
+// console.log(str)
+// console.log(str.split(""))
+// console.log(str.trim())
+console.log(str.split("").reverse().join(""));
+// split array banata h phir reverse reverse kar deta h phir joi waps string bana deta h
